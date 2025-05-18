4fe99642370aea295a4a9f3a5b739930519468ed
Hemit Patel <hemitvpatel@gmail.com>
2025-05-18 17:47:11 -0400

    switched to callback from reg function syntax (recommended)

--- apps/mail/components/ui/skeleton.tsx
+++ apps/mail/components/ui/skeleton.tsx
@@ -1,7 +1,8 @@
+import React from 'react';
 import { cn } from '@/lib/utils';
 
-function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+const Skeleton: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({ className, ...props }) => {
   return <div className={cn('bg-muted animate-pulse rounded-md', className)} {...props} />;
-}
+};
 
 export { Skeleton };
